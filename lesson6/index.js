36e1b9011550b697bf7e54c62ada5d07a947d723
shanto roy <shantosr3559@gmail.com>
2026-06-03 12:17:15 +0600

    update dependencies and refactor index.js to include movies-names

--- lesson6/index.js
+++ lesson6/index.js
@@ -1,2 +1,7 @@
-const getRandomFruitsName = require('random-fruits-name')
-console.log(getRandomFruitsName('en'));
+// const getRandomFruitsName = require('random-fruits-name')
+// console.log(getRandomFruitsName('en'));
+
+
+var moviesNames = require('movies-names');
+moviesNames.all // returns all movies
+console.log(moviesNames.random()); //returns a random movie name
